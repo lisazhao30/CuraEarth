b7a2a781166d872bd77bae9941fe2d1d5270b3aa
Kelly Pham <86501308+phamkelly17@users.noreply.github.com>
2022-01-08 11:50:50 -0500

    added vertical lines and fixed navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar/Navbar.js'
 function App() {
   return (
     <div className="App">
+      <span id = "vert-line"></span>
+      <span id = "vert-line2"></span>
       <Navbar></Navbar>
     </div>
   );
