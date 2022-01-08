892741456bb4c80531479efb366058a68a320be1
Lisa Zhao <84100023+lisazhao30@users.noreply.github.com>
2022-01-08 11:41:42 -0500

    more about us stuff

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import { default as logo1 } from '../images/curaeath-logo-1-3.svg';
+import '../css/about.css';
 
 const about = () => {
     return (
-        <div className="description">
-            <p>
-                Description goes here
-            </p>
+        <div className="about-section">
+            <div className="title">
+                <h1>
+                    Our Mission
+                </h1>
+            </div>
+            <div className="description">
+                <p>
+                    Description goes here
+                </p>
+            </div>
+            <div className="logo">
+                <img src={logo1} />
+            </div>
+            <div className="logo2">
+                <img src={logo1} />
+            </div>
+            <div className="logo3">
+                <img src={logo1} />
+            </div>
+            <div className="logo4">
+                <img src={logo1} />
+            </div>
         </div>
     )
 }
